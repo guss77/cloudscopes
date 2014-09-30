e563ec986c4a96c7e050285bea35681ce7bcf2b6
Oded Arbel <oded@showbox.com>
2014-09-30 12:54:51 +0300

    add listing of sockets and filtering by the open server port

--- lib/cloudscopes/network.rb
+++ lib/cloudscopes/network.rb
@@ -24,6 +24,31 @@ module Cloudscopes
       end
     end
     
+    def proc_ip_hex_to_dotted(hexstr)
+      hexstr.scan(/../).map(&:hex).reverse*"."
+    end
+    
+    def sockets
+      File.read("/proc/net/tcp").split("\n").drop(1).collect do |l| 
+        l.strip.split(/\s+/) 
+      end.collect do |s|
+        s[1..2] = s[1].split(/:/) + s[2].split(/:/) # split local/remote addresses to ip and port as separate fields
+        s[1] = proc_ip_hex_to_dotted(s[1])
+        s[2] = s[2].to_i(16)
+        s[3] = proc_ip_hex_to_dotted(s[3])
+        s[4] = s[4].to_i(16)
+        s.drop(1) # drop the index
+      end
+    end
+    
+    def open_server_ports(port)
+      sockets.select do |s|
+        s[4].to_i == 1 # only established sockets
+      end.select do |s| 
+        s[1] == port
+      end
+    end
+    
   end
   
 end
