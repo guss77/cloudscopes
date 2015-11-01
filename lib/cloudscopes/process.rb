1114f72b1c2d56a95a95d13d27e7a3d3bdbdf1cb
Oded Arbel <oded@showbox.com>
2015-11-01 13:42:48 +0200

    better interface for process tracking with support for reading all "standard file"-like process field. SystemProcess::mem_usage based on a naive algorithm that ignores shared memory pages

--- lib/cloudscopes/process.rb
+++ lib/cloudscopes/process.rb
@@ -14,13 +14,25 @@ module Cloudscopes
         "/proc/#{@id}/#{field}"
       end
       
+      def method_missing(name, *args)
+        raise ArgumentError.new("wrong number of arguments (#{args.length} for 0)") unless args.length == 0
+        begin
+          File.read(procpath(name.to_s))
+        rescue Errno::ENOENT => e # ignore kernel threads
+          ''
+        rescue SystemCallError => e # report and ignore
+          $stderr.puts "Error accessing process #{@id}: #{e.class}:#{e.message}"
+          ''
+        end
+      end
+      
       def exe
         begin
           File.readlink(procpath('exe'))
         rescue Errno::ENOENT => e # ignore kernel threads
           ''
         rescue SystemCallError => e # report and ignore
-          $stderr.puts "Error accessing process #{@id}: #{e.message}"
+          $stderr.puts "Error accessing process #{@id}: #{e.class}:#{e.message}"
           ''
         end
       end
@@ -40,10 +52,43 @@ module Cloudscopes
       def user
         Etc.getpwuid(uid || 0).name
       end
+      
+      def mem_usage
+        # lots of magic follow, peruse at your own risk
+        maps.strip.split("\n").collect do |l|
+          l.split(/\s+/)
+        end.select do |r|
+          r[4].to_i == 0 && r[1] !~ /s/
+        end.collect do |r|
+          b,e = r[0].split(/-/,2).collect{ |a| a.to_i(16) }
+          r[0] = e - b
+          r
+        end.inject(0) do |s,r|
+          s += r[0]
+        end
+      end
     end
     
     def list
-      Dir["/proc/[0-9]*[0-9]"].collect{|dir| SystemProcess.new(File.basename(dir).to_i) }
+      list = Dir["/proc/[0-9]*[0-9]"].collect{|dir| SystemProcess.new(File.basename(dir).to_i) }
+      list.define_singleton_method(:method_missing) do |name, *args|
+        case name.to_s
+        when /^by_(.*)/
+          field = $1.to_sym
+          raise ArgumentError.new("wrong number of arguments (#{args.length} for 1)") unless args.length == 1
+          select do |ps| 
+            case ps.send(field)
+            when args.first
+              true
+            else
+              false
+            end
+          end
+        else
+          raise NoMethodError.new("No such method #{name}",name)
+        end
+      end
+      list
     end
     
   end
--- lib/cloudscopes/version.rb
+++ lib/cloudscopes/version.rb
@@ -1,3 +1,3 @@
 module Cloudscopes
-  VERSION = "0.10.4"
+  VERSION = "0.11.0"
 end
