8b0eddf910e84abfeb7a906836b0777475cdb9af
Tilo Prütz <tilo@infopark.de>
2017-04-20 10:03:31 +0200

    support for metrics configuration directory

--- lib/cloudscopes/configuration.rb
+++ lib/cloudscopes/configuration.rb
@@ -8,7 +8,11 @@ module Cloudscopes
       @opts = Cloudscopes::Options.new
       configuration = YAML.load(File.read(@opts.config_file))
       @settings = configuration['settings']
-      configuration['metrics']
+      @metrics = configuration['metrics'] || {}
+      if metric_dir = @settings['metric_definition_dir']
+        merge_metric_definitions(Dir.glob("#{metric_dir}/*").select(&File.method(:file?)))
+      end
+      @metrics
     end
 
     def should_publish
@@ -27,6 +31,17 @@ module Cloudscopes
 
     def data_dimensions
       @settings['dimensions'] || ({ 'InstanceId' => '#{ec2.instance_id}' })
+    end
+
+    private
+
+    def merge_metric_definitions(files)
+      files.each do |metric_file|
+        YAML.load(File.read(metric_file)).each do |namespace, definitions|
+          @metrics[namespace] ||= []
+          @metrics[namespace] += definitions
+        end
+      end
     end
   end
 end
