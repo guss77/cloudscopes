f80f84e55eaeb7be4dac1fce050fc67863f7a6e2
Tilo Prütz <tilo@infopark.de>
2017-05-31 12:09:56 +0200

    removed some whitespace

--- lib/cloudscopes/memory.rb
+++ lib/cloudscopes/memory.rb
@@ -1,22 +1,18 @@
 module Cloudscopes
-  
   class Memory
-
     File.read('/proc/meminfo').split("\n").collect do |line|
       line =~ /(\w+):\s+(\d+)/ and { name: $1, value: $2.to_i } 
     end.each do |data|
       next if data.nil? 
       define_method(data[:name]) { data[:value] }
     end
-    
+
     def MemAvailable
       return self.MemFree + self.Buffers + self.Cached
     end
-    
+
     def MemUsed
       return self.MemTotal - self.MemFree - self.Buffers - self.Cached
     end
-
   end
-  
 end
